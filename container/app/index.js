8574e6921ceca675dc5e0fcc630c34caa4ce5986
jonathanssandberg-alt <jonathanssandberg@gmail.com>
2025-11-20 11:21:39 +0100

    Trigger container CI with small app change

--- container/app/index.js
+++ container/app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from the containerized Node.js app!//jonathan");
+  res.send("Hello from the containerized Node.js app!//jonathan2.0");
 });
 
 app.get("/health", (req, res) => {
